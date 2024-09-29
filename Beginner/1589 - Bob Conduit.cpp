d22dc4c6db523417825b1a10247f2c4a470e0e05
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2024-09-29 15:10:51 +0600

    Create 1589 - Bob Conduit.cpp

---
+++ Beginner/1589 - Bob Conduit.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int r1, r2;
+        cin >> r1 >> r2;
+        
+        cout << r1 + r2 << endl;
+    }
+    
+    return 0;
+}
