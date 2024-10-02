6f09cd88813be62bd92f8d0b3057c88fe84f0344
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2024-10-02 13:39:20 +0600

    Create 1864 - Our Days Are Never Coming Back.cpp

---
+++ Beginner/1864 - Our Days Are Never Coming Back.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n ; i++){
+        cout << s[i];
+    }
+
+    cout << endl;
+    return 0;
+}
